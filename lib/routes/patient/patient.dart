7274fa0657b44d2aa2e025c3eab4f932dd24902e
tgeng1 <geng.tian@jsure.com>
2020-04-10 17:37:01 +0800

    modify load more

--- lib/routes/patient/patient.dart
+++ lib/routes/patient/patient.dart
@@ -11,8 +11,8 @@ class Patient extends StatefulWidget {
 
 class _PatientState extends State<Patient> with AutomaticKeepAliveClientMixin{
   ScrollController _controller = new ScrollController();
-  static List<dynamic> _subjectList = [];
-  static int _subjectCount = 0;
+  List<dynamic> _subjectList = [];
+  int _subjectCount = 0;
   static var today = DateTime.now();
   static String _trialID = '';
   static var _startTime = today.subtract(Duration(days: 4200)).millisecondsSinceEpoch;
@@ -26,12 +26,21 @@ class _PatientState extends State<Patient> with AutomaticKeepAliveClientMixin{
 
   @override
   initState() {
+    super.initState();
+    _getSubjectList();
     _controller.addListener(() {
       if (_controller.position.pixels == _controller.position.maxScrollExtent) {
-        _getSubjectList(true);
+        if (_subjectList.length == _subjectCount) {
+          print('到底了.....');
+        } else {
+          setState(() {
+            patientPageNo += 1;
+          });
+          _getSubjectList(true);
+        }
       }
     });
-    super.initState();
+    
   }
 
   @override
@@ -55,12 +64,19 @@ class _PatientState extends State<Patient> with AutomaticKeepAliveClientMixin{
     Map<String, dynamic> _result = await PatientApi.getSubjects(_dataInfo);
     if (_result.isNotEmpty && _result['code'] == 'success') {
       List<dynamic> _subjectListInfo = _result['payload']['trialSubjects'];
+      int _count = _result['payload']['totalCount'];
       if (_isLoadMore) {
-        _subjectList.addAll(_subjectListInfo);
+        setState(() {
+          _subjectList.addAll(_subjectListInfo);
+          _subjectCount = _count;
+          _isLoadMore = false;
+        });
       } else {
-        _subjectList = _subjectListInfo;
+        setState(() {
+          _subjectList = _subjectListInfo;
+          _subjectCount = _count;
+        });
       }
-      return _subjectListInfo;
     }
   }
 
@@ -91,89 +107,98 @@ class _PatientState extends State<Patient> with AutomaticKeepAliveClientMixin{
     };
   }
 
-  Widget _text(_text) {
+  Widget _text(_text, _weight, _size) {
     return Text(
       _text,
       style: TextStyle(
         color: Colors.white,
-        fontWeight: FontWeight.w800
+        fontWeight: _weight,
+        fontSize: _size
       ),
     );
   }
 
-  Widget _subjectItem(_subjectInfo) {
-    String _subjectName = _subjectInfo['subject']['name'] ?? '--';
-    Map<String, dynamic> _currentStage = _subjectInfo['currentStage'] ?? {};
-    Map<String, dynamic> _trialInfo = _subjectInfo['trial'] ?? {};
+  Widget _subjectItem(_subjectList, _index) {
+    if (_index < _subjectList.length) {
+      var _subjectInfo = _subjectList[_index];
+      String _subjectName = _subjectInfo['subject']['name'] ?? '--';
+      Map<String, dynamic> _currentStage = _subjectInfo['currentStage'] ?? {};
+      Map<String, dynamic> _trialInfo = _subjectInfo['trial'] ?? {};
 
-    String _uTime = _subjectInfo['utime'];
-    var _date = _uTime != null ? formatDate(DateTime.parse(_uTime), [yyyy, '-', mm, '-', dd]) :'--';
+      String _uTime = _subjectInfo['utime'];
+      var _date = _uTime != null ? formatDate(DateTime.parse(_uTime), [yyyy, '-', mm, '-', dd]) :'--';
 
-    String _agent = _renderAgent(_subjectInfo)['agent'];
+      String _agent = _renderAgent(_subjectInfo)['agent'];
 
-    String _stageState = '成功';
-    List<Color> _cardColor = [Color(0xFFD37F13), Color(0xFFCFAF02)];
+      String _stageState = '成功';
+      List<Color> _cardColor = [Color(0xFFD37F13), Color(0xFFCFAF02)];
 
-    if (_currentStage.isNotEmpty) {
-      if (_currentStage['code'] == 4) {
-        _cardColor = [Color(0xFF01AF88), Color(0xFF15C589)];
+      if (_currentStage.isNotEmpty) {
+        if (_currentStage['code'] == 4) {
+          _cardColor = [Color(0xFF01AF88), Color(0xFF15C589)];
+        }
+        if (_currentStage['result'] == 'failed') {
+          _cardColor = [Color(0xFF8F959C), Color(0xFFA5AFB8)];
+          _stageState = '失败';
+        }
+        if (_currentStage['result'] == 'ongoing') {
+          _cardColor = [Color(0xFF1A7CBA), Color(0xFF1B9FB9)];
+          _stageState = '有待跟进';
+        }
       }
-      if (_currentStage['result'] == 'failed') {
-        _cardColor = [Color(0xFF8F959C), Color(0xFFA5AFB8)];
-        _stageState = '失败';
-      }
-      if (_currentStage['result'] == 'ongoing') {
-        _cardColor = [Color(0xFF1A7CBA), Color(0xFF1B9FB9)];
-        _stageState = '有待跟进';
-      }
-    }
-    return Container(
-      margin: const EdgeInsets.only(left: 15, right: 15, top: 15),
-      padding: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(8.0)),
-        gradient: LinearGradient(
-          colors: _cardColor,
-          begin: FractionalOffset(0, 0),
-          end: FractionalOffset(0, 1) 
-        )
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _text(_subjectName),
-                _text(_trialInfo['name'] ?? '--'),
-                _text(_date),
-                _text('负责人: $_agent'),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  flex: 2,
-                  child: Image.asset('images/patientImages/step1.png', width: 30.0, height: 30.0,),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: _text(_currentStage['name'] + _stageState),
-                )
-              ],
-            ),
+      return Container(
+        height: 135.0,
+        margin: const EdgeInsets.only(left: 15, right: 15, top: 15),
+        padding: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          gradient: LinearGradient(
+            colors: _cardColor,
+            begin: FractionalOffset(0, 0),
+            end: FractionalOffset(0, 1) 
           )
-        ],
-      ),
-    );
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _text(_subjectName, FontWeight.w900, 16.0),
+                  _text(_trialInfo['name'] ?? '--', FontWeight.w600, 14.0),
+                  _text(_date, FontWeight.w600, 15.0),
+                  _text('负责人: $_agent', FontWeight.w900, 15.0),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Image.asset('images/patientImages/step1.png', width: 30.0, height: 30.0,),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: _text(_currentStage['name'] + _stageState, FontWeight.w900, 14.0),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    } else {
+      return Container(
+        child: Text('加载更多....'),
+      );
+    }
   }
 
   @override
@@ -189,33 +214,25 @@ class _PatientState extends State<Patient> with AutomaticKeepAliveClientMixin{
         ],
       ),
       drawer: Personal(),
-      body: RefreshIndicator(
-        child: FutureBuilder(
-          future: _getSubjectList(),
-          initialData: [],
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Text("Error: ${snapshot.error}");
-              } else {
-                return ListView.builder(
-                  controller: _controller,
-                  shrinkWrap: true,
-                  itemCount: snapshot.data.length,
-                  itemExtent: 155.0,
-                  itemBuilder: (BuildContext context, int index) {
-                    return _subjectItem(snapshot.data[index]);
-                  }
-                );
-              }
-            } else {
-              // loading
-              return CustomLoading(text: '加载中.....',);
+      body: _subjectList.isEmpty
+      ? CustomLoading(text: '加载中....',)
+      : RefreshIndicator(
+          child: ListView.builder(
+            controller: _controller,
+            // shrinkWrap: true,
+            itemCount: _subjectList.length + 1,
+            // itemExtent: 155.0,
+            itemBuilder: (BuildContext context, int index) {
+              return _subjectItem(_subjectList, index);
             }
+          ),
+          onRefresh: () {
+            setState(() {
+              patientPageNo = 1;
+            });
+            return _getSubjectList();
           },
-        ),
-        onRefresh: _getSubjectList,
-      )
+        )
     );
   }
 }
